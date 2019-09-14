396cb45306f47e37637e6e8d5f349c90a18519df
Chris Fowler <chris.fowler73@hotmail.co.uk>
2019-09-14 16:22:17 +0100

    Creating a user profile page

--- django_auth/urls.py
+++ django_auth/urls.py
@@ -1,6 +1,6 @@
 from django.contrib import admin
 from django.urls import path
-from accounts.views import index, logout, login, register
+from accounts.views import index, logout, login, register, user_profile
 
 urlpatterns = [
     path('admin/', admin.site.urls),
@@ -8,4 +8,5 @@ urlpatterns = [
     path('accounts/logout/', logout, name='logout'),
     path('accounts/login/', login, name='login'),
     path('accounts/register/', register, name='register'),
+    path('accounts/profile/', user_profile, name='profile'),
 ]
