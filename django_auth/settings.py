9db75df1e0439ab573e2b947f88a677c4f175dbc
Chris Fowler <chris.fowler73@hotmail.co.uk>
2019-09-15 11:20:44 +0100

    Created templates for password reset

--- django_auth/settings.py
+++ django_auth/settings.py
@@ -27,8 +27,7 @@ DEBUG = True
 
 ALLOWED_HOSTS = []
 
-if DEBUG:
-    EMAIL_BACKEND = "django.core.mail.backends.console.EmailBackend"
+
 
 
 # Application definition
@@ -125,7 +124,16 @@ STATIC_URL = '/static/'
 
 MESSAGE_STORAGE = "django.contrib.messages.storage.session.SessionStorage"
 
+EMAIL_BACKEND = "django.core.mail.backends.console.EmailBackend"
+# EMAIL_BACKEND = "django.core.mail.backends.filebased.EmailBackend"
+
+# EMAIL_USE_TLS = True
+# EMAIL_HOST = 'smtp.gmail.com'
+# EMAIL_HOST_USER = os.environ.get("EMAIL_ADDRESS")
+# EMAIL_HOST_PASSWORD = os.environ.get("EMAIL_PASSWORD")
+# EMAIL_PORT = 587
+
 try:
-    from .local_settings import SECRET_KEY
+    from .local_settings import *
 except ImportError:
     pass
